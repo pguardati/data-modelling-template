a70e3b41fe1e85afeb4819903de9690ada3a5134
PietroGuardati <pietroguardati@gmail.com>
2021-03-12 22:32:47 +0100

    refactor: refactor sql style

--- src/etl.sql
+++ src/etl.sql
@@ -1,122 +1,194 @@
 -- drop tables
-drop table if exists education_titles cascade;
-drop table if exists job_titles cascade;
-drop table if exists managers cascade;
-drop table if exists departments cascade;
-drop table if exists offices cascade;
-drop table if exists contracts cascade;
-drop table if exists employees cascade;
+DROP TABLE IF EXISTS education_titles CASCADE;
+
+DROP TABLE IF EXISTS job_titles CASCADE;
+
+DROP TABLE IF EXISTS managers CASCADE;
+
+DROP TABLE IF EXISTS departments CASCADE;
+
+DROP TABLE IF EXISTS offices CASCADE;
+
+DROP TABLE IF EXISTS contracts CASCADE;
+
+DROP TABLE IF EXISTS employees CASCADE;
 
 -- create tables
-
-create table education_titles (
-    title_id serial primary key,
+CREATE TABLE education_titles (
+    title_id serial PRIMARY KEY,
     title_name varchar(100)
 );
 
-create table job_titles (
-    job_id serial primary key,
+CREATE TABLE job_titles (
+    job_id serial PRIMARY KEY,
     job_name varchar(100)
 );
 
-create table managers (
-    manager_id serial primary key,
+CREATE TABLE managers (
+    manager_id serial PRIMARY KEY,
     manager_name varchar(100)
 );
 
-create table departments (
-    department_id serial primary key,
+CREATE TABLE departments (
+    department_id serial PRIMARY KEY,
     department_name varchar(100)
 );
 
-create table offices (
-    office_id serial primary key,
+CREATE TABLE offices (
+    office_id serial PRIMARY KEY,
     office_name varchar(50),
     address varchar(100),
     city varchar(50),
     state varchar(2)
 );
 
-create table contracts (
-    contract_id serial primary key,
+CREATE TABLE contracts (
+    contract_id serial PRIMARY KEY,
     employee_id varchar(50),
     hire_date date,
     start_date date,
     end_date date,
     salary int,
-    job_id int references job_titles(job_id),
-    manager_id int references managers(manager_id),
-    department_id int references departments(department_id),
-    office_id int references offices(office_id)
+    job_id int REFERENCES job_titles(job_id),
+    manager_id int REFERENCES managers(manager_id),
+    department_id int REFERENCES departments(department_id),
+    office_id int REFERENCES offices(office_id)
 );
 
-create table employees (
-    employee_id varchar(8) primary key,
+CREATE TABLE employees (
+    employee_id varchar(8) PRIMARY KEY,
     employee_name varchar(50),
     employee_email varchar(100),
-    title_id int references education_titles(title_id),
-    current_contract_id int references contracts(contract_id)
+    title_id int REFERENCES education_titles(title_id),
+    current_contract_id int REFERENCES contracts(contract_id)
 );
 
 -- education_titles
-insert into education_titles (title_name)
-select distinct education_lvl from proj_stg;
+INSERT INTO
+    education_titles (title_name)
+SELECT
+    DISTINCT education_lvl
+FROM
+    proj_stg;
 
 -- job_titles
-insert into job_titles (job_name)
-select distinct job_title from proj_stg;
+INSERT INTO
+    job_titles (job_name)
+SELECT
+    DISTINCT job_title
+FROM
+    proj_stg;
 
 -- managers
-insert into managers (manager_name)
-select distinct manager from proj_stg;
+INSERT INTO
+    managers (manager_name)
+SELECT
+    DISTINCT manager
+FROM
+    proj_stg;
 
 -- departments
-insert into departments (department_name)
-select distinct department_nm from proj_stg;
+INSERT INTO
+    departments (department_name)
+SELECT
+    DISTINCT department_nm
+FROM
+    proj_stg;
 
 -- offices
-insert into offices (office_name, address, city, state)
-select distinct location, address, city, state from proj_stg;
+INSERT INTO
+    offices (office_name, address, city, state)
+SELECT
+    DISTINCT location,
+    address,
+    city,
+    state
+FROM
+    proj_stg;
 
 -- contracts
-insert into contracts (employee_id, hire_date, start_date, end_date, salary, job_id, manager_id, department_id, office_id)
-select distinct emp_id, hire_dt, start_dt, end_dt, salary, j.job_id, m.manager_id, d.department_id, o.office_id
-from proj_stg as staging
-join job_titles as j
-on staging.job_title = j.job_name
-join managers as m
-on staging.manager = m.manager_name
-join departments as d
-on staging.department_nm = d.department_name
-join offices as o
-on staging."location" = o.office_name;
+INSERT INTO
+    contracts (
+        employee_id,
+        hire_date,
+        start_date,
+        end_date,
+        salary,
+        job_id,
+        manager_id,
+        department_id,
+        office_id
+    )
+SELECT
+    DISTINCT emp_id,
+    hire_dt,
+    start_dt,
+    end_dt,
+    salary,
+    j.job_id,
+    m.manager_id,
+    d.department_id,
+    o.office_id
+FROM
+    proj_stg AS staging
+    JOIN job_titles AS j ON staging.job_title = j.job_name
+    JOIN managers AS m ON staging.manager = m.manager_name
+    JOIN departments AS d ON staging.department_nm = d.department_name
+    JOIN offices AS o ON staging."location" = o.office_name;
 
 -- employees
-insert into employees
-(employee_id, employee_name, employee_email, title_id, current_contract_id)
-with employees_updated as (
-    -- select most recent record of each employee
-    select *
-    from (
-        select *, row_number() over ( partition by emp_id order by start_dt desc) as contract_number
-        from proj_stg
-       )
-    as staging
-    where contract_number=1),
-contracts_updated as (
-    -- select most recent contract of each employee
-    select *
-    from (
-        select *, row_number() over ( partition by employee_id order by start_date desc) as contract_number
-        from contracts
-       )
-    as staging
-    where contract_number=1
-)
-select distinct
-    Emp_ID, Emp_NM, Email, e.title_id, c.contract_id as current_contract_id
-from employees_updated as staging
-join education_titles as e
-on staging.education_lvl = e.title_name
-join contracts_updated as c
-on staging.emp_id = c.employee_id
+INSERT INTO
+    employees (
+        employee_id,
+        employee_name,
+        employee_email,
+        title_id,
+        current_contract_id
+    ) WITH employees_updated AS (
+        -- select most recent record of each employee
+        SELECT
+            *
+        FROM
+            (
+                SELECT
+                    *,
+                    row_number() over (
+                        PARTITION by emp_id
+                        ORDER BY
+                            start_dt DESC
+                    ) AS contract_number
+                FROM
+                    proj_stg
+            ) AS staging
+        WHERE
+            contract_number = 1
+    ),
+    contracts_updated AS (
+        -- select most recent contract of each employee
+        SELECT
+            *
+        FROM
+            (
+                SELECT
+                    *,
+                    row_number() over (
+                        PARTITION by employee_id
+                        ORDER BY
+                            start_date DESC
+                    ) AS contract_number
+                FROM
+                    contracts
+            ) AS staging
+        WHERE
+            contract_number = 1
+    )
+SELECT
+    DISTINCT Emp_ID,
+    Emp_NM,
+    Email,
+    e.title_id,
+    c.contract_id AS current_contract_id
+FROM
+    employees_updated AS staging
+    JOIN education_titles AS e ON staging.education_lvl = e.title_name
+    JOIN contracts_updated AS c ON staging.emp_id = c.employee_id
--- src/query.sql
+++ src/query.sql
@@ -1,52 +1,87 @@
 -- Q1 - query employee by name
-with contracts_roles_and_departments as (
-    select contract_id, j.job_name, d.department_name from contracts as c
-    join job_titles as j
-    on c.job_id = j.job_id
-    join departments as d
-    on c.department_id = d.department_id
+WITH contracts_roles_and_departments AS (
+    SELECT
+        contract_id,
+        j.job_name,
+        d.department_name
+    FROM
+        contracts AS c
+        JOIN job_titles AS j ON c.job_id = j.job_id
+        JOIN departments AS d ON c.department_id = d.department_id
 )
-select employee_name, job_name, department_name from employees as e
-join contracts_roles_and_departments as c
-on e.current_contract_id = c.contract_id
-order by department_name
+SELECT
+    employee_name,
+    job_name,
+    department_name
+FROM
+    employees AS e
+    JOIN contracts_roles_and_departments AS c ON e.current_contract_id = c.contract_id
+ORDER BY
+    department_name
 
 -- Q2 - Create Web programmer as title
-insert into job_titles
-values (default, 'Web Programmer')
+INSERT INTO
+    job_titles
+VALUES
+    (DEFAULT, 'Web Programmer')
 
 -- Q3 - Correct from Web programmer to web developer
-update job_titles
-set job_name = 'Web Developer'
-where job_name = 'Web Programmer'
+UPDATE
+    job_titles
+SET
+    job_name = 'Web Developer'
+WHERE
+    job_name = 'Web Programmer'
 
 -- Q4 - Delete Web Developer from database
-delete from job_titles
-where job_name = 'Web Developer'
+DELETE FROM
+    job_titles
+WHERE
+    job_name = 'Web Developer'
 
 -- Q5 - How many employees are in each department?
-with employees_and_departments as (
-    select contract_id, d.department_name from contracts as c
-    join departments as d
-    on c.department_id = d.department_id
+WITH employees_and_departments AS (
+    SELECT
+        contract_id,
+        d.department_name
+    FROM
+        contracts AS c
+        JOIN departments AS d ON c.department_id = d.department_id
 )
-select department_name, count(*) from employees_and_departments
-group by department_name
-order by count
+SELECT
+    department_name,
+    count(*)
+FROM
+    employees_and_departments
+GROUP BY
+    department_name
+ORDER BY
+    count
 
 -- Q6 - Current and past job of Toni Lembeck
-with explicit_contracts as (
-    select employee_id, job_name, department_name, manager_name, start_date, end_date from contracts as c
-    join job_titles as j
-    on c.job_id = j.job_id
-    join departments as d
-    on c.department_id = d.department_id
-    join managers as m
-    on c.manager_id = m.manager_id
-)
-select
-employee_name, job_name, department_name, manager_name, start_date, end_date
-from employees as e
-join explicit_contracts as c
-on e.employee_id =  c.employee_id
-where employee_name = 'Toni Lembeck'
+    WITH explicit_contracts AS (
+        SELECT
+            employee_id,
+            job_name,
+            department_name,
+            manager_name,
+            start_date,
+            end_date
+        FROM
+            contracts AS c
+            JOIN job_titles AS j ON c.job_id = j.job_id
+            JOIN departments AS d ON c.department_id = d.department_id
+            JOIN managers AS m ON c.manager_id = m.manager_id
+    )
+SELECT
+    employee_name,
+    job_name,
+    department_name,
+    manager_name,
+    start_date,
+    end_date
+FROM
+    employees AS e
+    JOIN explicit_contracts AS c ON e.employee_id = c.employee_id
+WHERE
+    employee_name = 'Toni Lembeck'
